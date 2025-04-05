5c2e3cfd2595f57ffd50eef63c9481ab39b3f19c
emon <tj.emon33@gmail.com>
2025-04-05 09:46:46 +0600

    step --> useReducer

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,16 +1,29 @@
 import { useReducer, useState } from "react";
+const initialState = { count: 0, step: 1 };
 
 function reducer(state, action) {
-  console.log(action, state);
-  if (action.type === "inc") return state + 1;
-  if (action.type === "dec") return state - 1;
-  if (action.type === "setCount") return action.payload;
+  switch (action.type) {
+    case "inc":
+      return { ...state, count: state.count + state.step };
+    case "dec":
+      return { ...state, count: state.count - state.step };
+    case "setCount":
+      return { ...state, count: action.payload };
+    case "setStep":
+      return { ...state, step: action.payload };
+    case "reset":
+      return initialState;
+    default:
+      throw new Error("unknown action type");
+  }
 }
 
 function DateCounter() {
+  // const [step, setStep] = useState(1);
   // const [count, setCount] = useState(0);
-  const [count, dispatch] = useReducer(reducer, 0);
-  const [step, setStep] = useState(1);
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { count, step } = state;
 
   // This mutates the date object.
   const date = new Date("june 21 2027");
@@ -18,28 +31,33 @@ function DateCounter() {
 
   const dec = function () {
     // setCount((count) => count - 1);
-    //   setCount((count) => count - step);
+    // setCount((count) => count - step);
+
     dispatch({ type: "dec" });
   };
 
   const inc = function () {
-    dispatch({ type: "inc" });
     // setCount((count) => count + 1);
     // setCount((count) => count + step);
+
+    dispatch({ type: "inc" });
   };
 
   const defineCount = function (e) {
     // setCount(Number(e.target.value));
+
     dispatch({ type: "setCount", payload: Number(e.target.value) });
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    dispatch({ type: "setStep", payload: Number(e.target.value) });
   };
 
   const reset = function () {
     // setCount(0);
-    setStep(1);
+    // setStep(1);
+
+    dispatch({ type: "reset" });
   };
 
   return (
